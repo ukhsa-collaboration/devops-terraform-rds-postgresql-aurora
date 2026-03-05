689c1a6e2c4d0b1bc0abce6dcc519ba92429d35d
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2026-03-05 18:04:14 +0000

    fix: correct output for master secret arn

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "db_master_secret_arn" {
   description = "Secrets Manager ARN containing Aurora master username/password."
-  value       = module.cluster.cluster_master_user_secret.secret_arn
+  value       = try(module.cluster.cluster_master_user_secret[0].secret_arn, null)
 }
