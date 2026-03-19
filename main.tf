f96881508b98f666b7deab768d1df4909e9744cf
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2026-03-19 17:44:11 +0000

    lint: terraform fmt

--- main.tf
+++ main.tf
@@ -215,7 +215,7 @@ resource "aws_rds_cluster" "this" {
 
   lifecycle {
     # These are managed by AWS Backup and can cause unnecessary diffs if AWS Backup changes the preferred backup window.
-    ignore_changes = [ preferred_backup_window, backup_retention_period ]
+    ignore_changes = [preferred_backup_window, backup_retention_period]
   }
 }
 
